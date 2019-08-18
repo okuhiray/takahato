099e431819e638dfa710edba856d8e73d9c9c635
okuhiray <yusuke_okuhira@biglobe.jp>
2019-08-18 16:47:26 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,19 @@
-// 食物連鎖のシミュレータ
-const INIT = 16; // 生物の初期サイズ
-const MIN = 12; // 生存最小サイズ
-const MAX = 128; // 成熟サイズ
-const NEAR = 100; // 隣接判定距離
-const DENSITY = 8; // 共存可能個体数
-const RANGE = 300; // 種子が飛散する最大距離
+// タカハトゲームのシミュレータ
+const STEP = 30;
+const R = 50;
+const MAX_WAIT = 30;
 
-let plants = new Plants();
+let creature;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  const p = createVector(windowWidth, windowHeight).div(2); // 画面中央
-  const plant = new Plant(p);
-  plants.add(plant);
+  creature = new Creature();
 }
 
 function draw() {
-<<<<<<< HEAD
-  background(0);
-  plants.update();
-  plants.draw();
-  textSize(32);
-  fill(255);
-  text(plants.size, 10, 50);
-=======
   background(255);
   if(mouseIsPressed){
     creature.move();
-<<<<<<< HEAD
-    creature.draw();   
-  }  
->>>>>>> develop
-=======
     creature.draw();
   }
->>>>>>> develop
 }
-
